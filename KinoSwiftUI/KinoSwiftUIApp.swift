1f6546c70ddd73c038825a8d629f67cf5fb3e047
Luka Lešić <lukalesic20@gmail.com>
2023-01-20 13:51:11 +0100

    Initial Commit

---
+++ KinoSwiftUI/KinoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinoSwiftUIApp.swift
+//  KinoSwiftUI
+//
+//  Created by Luka Lešić on 20.01.2023..
+//
+
+import SwiftUI
+
+@main
+struct KinoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
